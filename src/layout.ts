e897f8cd435599bccf237f6efa293c445147daba
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2024-03-03 14:29:19 -0600

    Deleted vite images and removed shadcn

---
+++ src/layout.ts
@@ -0,0 +1,14 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="grid h-screen place-items-center">
+    <h1>Vite + TypeScript</h1>
+    <p>
+      Lorum ipsum dolor et
+    </p>
+    <div class="card">
+      Button
+    </div>
+
+  </div>
+`;
